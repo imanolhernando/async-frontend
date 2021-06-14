85859ca60df6570a59b5ff726198602a442356d6
imanol <44357739+imanolhernando@users.noreply.github.com>
2021-06-14 18:33:31 +0200

    add: validator spec empty

--- src/app/validators/index.spec.ts
+++ src/app/validators/index.spec.ts
@@ -1,13 +1,21 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AbstractControl, FormControl } from '@angular/forms';
 import { urlValidator } from '.';
 
 describe('urlValidator', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      urlValidator
-      ]
-  }));
+  // beforeEach(() => TestBed.configureTestingModule({
+  //   providers: [
+  //     urlValidator
+  //   ],
+  //   declarations:[],
+  //   imports:[],
+  //   schemas:[ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+  // }).compileComponents());
+
+  // beforeEach(()=> {
+
+  // })
 
   it('should be created', () => {
     expect(urlValidator()).toBeTruthy();
